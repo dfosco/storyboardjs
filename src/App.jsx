b18f40bfb00a58578e4a0bc6b91bc2daf604b93e
Daniel Fosco <dfosco@github.com>
2025-01-26 00:11:33 +0100

    Basic whiteboard functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,46 @@
 import React, { useRef } from 'react';
 import Draggable from 'react-draggable';
+import './styles.css';
+import './draggable.css';
+
 
 function App() {
   const nodeRef = useRef(null);
   return (
-    <Draggable
-      nodeRef={nodeRef}
-      handle="#handle"
-      defaultPosition={{x: 0, y: 0}}
-      position={null}
-      grid={[25, 25]}
-      scale={1}>
-      <div style={{ border: '1px solid #999', padding: '20px' }} ref={nodeRef}>
-        <div id="handle" style={{ marginBottom: '10px' }}>
-          Drag here
-        </div>
-        <div>This is the content</div>
+    <main>
+      <div className="canvas">
+        <Draggable
+          nodeRef={nodeRef}
+          handle=".draggable"
+          position={null}
+          grid={[40, 40]}
+          bounds=".canvas"
+          offsetParent=""
+          scale={1}>
+          <div className="draggable" style={{ border: '1px solid #999', padding: '20px' }} ref={nodeRef}>
+            <div id="handle">
+              Drag here
+            </div>
+            <div>This is the content</div>
+          </div>
+        </Draggable>
+        <Draggable
+          nodeRef={nodeRef}
+          handle=".draggable"
+          position={null}
+          grid={[40, 40]}
+          bounds=".canvas"
+          offsetParent=""
+          scale={1}>
+          <div className="draggable" style={{ border: '1px solid #999', padding: '20px' }} ref={nodeRef}>
+            <div id="handle">
+              Drag here
+            </div>
+            <div>This is the content</div>
+          </div>
+        </Draggable>        
       </div>
-    </Draggable>
+    </main>
   );
 }
 
